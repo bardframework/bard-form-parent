2b6e23d893415d92d6365d9dbc540491f7b3575f
Vahid Zafari <va.zafari@gmail.com>
2022-06-05 14:36:46 +0430

    add EnumHeaderTemplate

---
+++ form-data-table/src/main/java/org/bardframework/form/table/header/EnumHeaderTemplate.java
@@ -0,0 +1,24 @@
+package org.bardframework.form.table.header;
+
+import org.apache.commons.lang3.EnumUtils;
+import org.bardframework.form.common.table.TableHeader;
+
+import java.util.Locale;
+
+public class EnumHeaderTemplate<T extends Enum<T>> extends TableHeaderTemplate<TableHeader, T> {
+    private final Class<T> enumClass;
+
+    public EnumHeaderTemplate(Class<T> enumClass) {
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public T parse(String value, Locale locale) {
+        return EnumUtils.getEnum(enumClass, value, null);
+    }
+
+    @Override
+    public Object format(T value, Locale locale) {
+        return null == value ? null : value.name();
+    }
+}
