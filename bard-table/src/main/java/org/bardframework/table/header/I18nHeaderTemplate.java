66e66bbe808286220133488f15677c06d8a65e03
Vahid Zafari <va.zafari@gmail.com>
2022-08-01 19:45:52 +0430

    add I18nHeaderTemplate

--- bard-table/src/main/java/org/bardframework/table/header/I18nHeaderTemplate.java
+++ bard-table/src/main/java/org/bardframework/table/header/I18nHeaderTemplate.java
@@ -5,10 +5,20 @@ import org.springframework.context.MessageSource;
 import java.util.Locale;
 
 public class I18nHeaderTemplate extends StringHeaderTemplate {
+    private String keyPrefix = "";
+
     public I18nHeaderTemplate() {
     }
 
     public Object format(String value, Locale locale, MessageSource messageSource) {
-        return messageSource.getMessage(value, new Object[0], value, locale);
+        return messageSource.getMessage(keyPrefix + value, new Object[0], value, locale);
+    }
+
+    public String getKeyPrefix() {
+        return keyPrefix;
+    }
+
+    public void setKeyPrefix(String keyPrefix) {
+        this.keyPrefix = keyPrefix;
     }
 }
