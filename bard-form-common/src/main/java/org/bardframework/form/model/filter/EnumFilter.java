ff0ec0f93a66377c70f519501318f55549232b38
Vahid Zafari <va.zafari@gmail.com>
2022-09-03 08:14:51 +0430

    add EnumFilter

---
+++ bard-form-common/src/main/java/org/bardframework/form/model/filter/EnumFilter.java
@@ -0,0 +1,10 @@
+package org.bardframework.form.model.filter;
+
+/**
+ * Filter class for {@link String} id filter.
+ *
+ * @see Filter
+ */
+public class EnumFilter<T extends Enum<T>> extends Filter<T, EnumFilter<T>> {
+
+}
