1d4a618ce445968f71916fcb24aed164be58714a
v.zafari <va.zafari@gmail.com>
2023-12-22 06:33:00 +0330

    2.8

--- bard-form-common/src/main/java/org/bardframework/form/field/input/NewPasswordField.java
+++ bard-form-common/src/main/java/org/bardframework/form/field/input/NewPasswordField.java
@@ -1,8 +1,16 @@
 package org.bardframework.form.field.input;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.bardframework.form.field.FieldType;
 
+@Getter
+@Setter
+@ToString
 public class NewPasswordField extends PasswordField {
+
+    private Boolean showConfirmPassword;
 
     @Override
     public FieldType getType() {
--- bard-form/src/main/java/org/bardframework/form/field/input/NewPasswordFieldTemplate.java
+++ bard-form/src/main/java/org/bardframework/form/field/input/NewPasswordFieldTemplate.java
@@ -10,14 +10,19 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-public class NewPasswordFieldTemplate extends PasswordFieldTemplate {
+public class NewPasswordFieldTemplate extends InputFieldTemplate<NewPasswordField, String> {
 
     public NewPasswordFieldTemplate(String name) {
         super(name);
     }
 
     @Override
-    public boolean isValid(String flowToken, PasswordField field, String value, Map<String, String> flowData) {
+    public String toValue(String value) {
+        return value;
+    }
+
+    @Override
+    public boolean isValid(String flowToken, NewPasswordField field, String value, Map<String, String> flowData) {
         if (StringUtils.isBlank(value)) {
             if (Boolean.TRUE.equals(field.getRequired())) {
                 log.debug("field [{}] is required, but it's value is empty", field.getName());
@@ -43,11 +48,12 @@ public class NewPasswordFieldTemplate extends PasswordFieldTemplate {
     }
 
     @Override
-    public void fill(FormTemplate formTemplate, PasswordField field, Map<String, String> values, Locale locale) throws Exception {
+    public void fill(FormTemplate formTemplate, NewPasswordField field, Map<String, String> values, Locale locale) throws Exception {
         super.fill(formTemplate, field, values, locale);
         field.setRegex(FormUtils.getFieldStringProperty(formTemplate, this, "regex", locale, values, this.getDefaultValues().getRegex()));
         field.setMinLength(FormUtils.getFieldIntegerProperty(formTemplate, this, "minLength", locale, values, this.getDefaultValues().getMinLength()));
         field.setMaxLength(FormUtils.getFieldIntegerProperty(formTemplate, this, "maxLength", locale, values, this.getDefaultValues().getMaxLength()));
+        field.setShowConfirmPassword(FormUtils.getFieldBooleanProperty(formTemplate, this, "showConfirmPassword", locale, values, this.getDefaultValues().getShowConfirmPassword()));
     }
 
     @Override
